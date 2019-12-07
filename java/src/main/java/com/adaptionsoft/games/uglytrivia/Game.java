501747e2ccf895c841e772f154a6c527cda750eb
Domício Medeiros <domicioam@gmail.com>
2019-12-07 17:23:15 +0000

    Adding some unit tests with junit.

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -35,8 +35,6 @@ public class Game {
 	}
 
 	public boolean add(String playerName) {
-		
-		
 	    players.add(playerName);
 	    places[howManyPlayers()] = 0;
 	    purses[howManyPlayers()] = 0;
@@ -74,7 +72,6 @@ public class Game {
 				}
 			
 		} else {
-		
 			places[currentPlayer] = places[currentPlayer] + roll;
 			if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
 			
@@ -85,31 +82,6 @@ public class Game {
 			askQuestion();
 		}
 		
-	}
-
-	private void askQuestion() {
-		if (currentCategory() == "Pop")
-			System.out.println(popQuestions.removeFirst());
-		if (currentCategory() == "Science")
-			System.out.println(scienceQuestions.removeFirst());
-		if (currentCategory() == "Sports")
-			System.out.println(sportsQuestions.removeFirst());
-		if (currentCategory() == "Rock")
-			System.out.println(rockQuestions.removeFirst());		
-	}
-	
-	
-	private String currentCategory() {
-		if (places[currentPlayer] == 0) return "Pop";
-		if (places[currentPlayer] == 4) return "Pop";
-		if (places[currentPlayer] == 8) return "Pop";
-		if (places[currentPlayer] == 1) return "Science";
-		if (places[currentPlayer] == 5) return "Science";
-		if (places[currentPlayer] == 9) return "Science";
-		if (places[currentPlayer] == 2) return "Sports";
-		if (places[currentPlayer] == 6) return "Sports";
-		if (places[currentPlayer] == 10) return "Sports";
-		return "Rock";
 	}
 
 	public boolean wasCorrectlyAnswered() {
@@ -132,11 +104,7 @@ public class Game {
 				if (currentPlayer == players.size()) currentPlayer = 0;
 				return true;
 			}
-			
-			
-			
 		} else {
-		
 			System.out.println("Answer was corrent!!!!");
 			purses[currentPlayer]++;
 			System.out.println(players.get(currentPlayer) 
@@ -165,5 +133,30 @@ public class Game {
 
 	private boolean didPlayerWin() {
 		return !(purses[currentPlayer] == 6);
+	}
+	
+	private void askQuestion() {
+		if (currentCategory() == "Pop")
+			System.out.println(popQuestions.removeFirst());
+		if (currentCategory() == "Science")
+			System.out.println(scienceQuestions.removeFirst());
+		if (currentCategory() == "Sports")
+			System.out.println(sportsQuestions.removeFirst());
+		if (currentCategory() == "Rock")
+			System.out.println(rockQuestions.removeFirst());		
+	}
+	
+	
+	private String currentCategory() {
+		if (places[currentPlayer] == 0) return "Pop";
+		if (places[currentPlayer] == 4) return "Pop";
+		if (places[currentPlayer] == 8) return "Pop";
+		if (places[currentPlayer] == 1) return "Science";
+		if (places[currentPlayer] == 5) return "Science";
+		if (places[currentPlayer] == 9) return "Science";
+		if (places[currentPlayer] == 2) return "Sports";
+		if (places[currentPlayer] == 6) return "Sports";
+		if (places[currentPlayer] == 10) return "Sports";
+		return "Rock";
 	}
 }
--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -4,10 +4,28 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.adaptionsoft.games.uglytrivia.Game;
+
 public class SomeTest {
 
 	@Test
-	public void true_is_true() throws Exception {
-		assertTrue(false);
+	public void shouldBePlayableWhenGameHasTwoPlayers() {
+		Game game = new Game();
+		addTwoPlayers(game);
+		assertTrue(game.isPlayable());
+		assertEquals(2, game.howManyPlayers());
+	}
+	
+	@Test
+	public void shouldNotBePlayableWhenGameHasOnePlayer() {
+		Game game = new Game();
+		game.add("Player 1");
+		assertFalse(game.isPlayable());
+		assertEquals(1, game.howManyPlayers());
+	}
+	
+	private void addTwoPlayers(Game game) {
+		game.add("Player 1");
+		game.add("Player 2");
 	}
 }
