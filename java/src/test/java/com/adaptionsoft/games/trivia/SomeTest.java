167348fd23bb61c0d82d591fbbe01d21eaa28fc1
Domício Medeiros <domicioam@gmail.com>
2019-12-08 10:04:10 +0000

    Adding approval tests. Converting to maven project.

--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -2,6 +2,12 @@ package com.adaptionsoft.games.trivia;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.approvaltests.Approvals;
+import org.approvaltests.reporters.DiffReporter;
+import org.approvaltests.reporters.UseReporter;
 import org.junit.Test;
 
 import com.adaptionsoft.games.uglytrivia.Game;
@@ -9,7 +15,11 @@ import com.adaptionsoft.games.uglytrivia.Game;
 public class SomeTest {
 
 	@Test
+	@UseReporter(DiffReporter.class)
 	public void shouldBePlayableWhenGameHasTwoPlayers() {
+		
+		Approvals.verify("Hello");
+		
 		Game game = new Game();
 		addTwoPlayers(game);
 		assertTrue(game.isPlayable());
@@ -22,6 +32,32 @@ public class SomeTest {
 		game.add("Player 1");
 		assertFalse(game.isPlayable());
 		assertEquals(1, game.howManyPlayers());
+	}
+	
+	@Test
+	@UseReporter(DiffReporter.class)
+	public void testGame() {
+		ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
+		PrintStream printStream = new PrintStream(byteOutputStream);
+		System.setOut(printStream);
+		
+		Game game = new Game();
+		addTwoPlayers(game);
+		// first turn
+		game.roll(1);
+		game.wasCorrectlyAnswered();
+		game.roll(1);
+		game.wrongAnswer();
+		// second turn
+		game.roll(1);
+		game.wasCorrectlyAnswered();
+		game.roll(1);
+		game.wasCorrectlyAnswered();
+		
+		System.out.flush();
+		String output = byteOutputStream.toString();
+		
+		Approvals.verify(output);
 	}
 	
 	private void addTwoPlayers(Game game) {
