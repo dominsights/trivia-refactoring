e81af8420c4d67bbddb6182433851321ac696572
Domício Medeiros <domicioam@gmail.com>
2019-12-08 11:43:58 +0000

    Increasing code coverage to 100% in the legacy class to be refactored.

--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -15,31 +15,34 @@ import com.adaptionsoft.games.uglytrivia.Game;
 public class SomeTest {
 
 	@Test
-	@UseReporter(DiffReporter.class)
 	public void shouldBePlayableWhenGameHasTwoPlayers() {
-		
-		Approvals.verify("Hello");
+		ByteArrayOutputStream byteOutputStream = setCustomOutStream();
 		
 		Game game = new Game();
 		addTwoPlayers(game);
 		assertTrue(game.isPlayable());
 		assertEquals(2, game.howManyPlayers());
+		
+		String output = getOutput(byteOutputStream);
+		Approvals.verify(output);
 	}
 	
 	@Test
 	public void shouldNotBePlayableWhenGameHasOnePlayer() {
+		ByteArrayOutputStream byteOutputStream = setCustomOutStream();
+		
 		Game game = new Game();
 		game.add("Player 1");
 		assertFalse(game.isPlayable());
 		assertEquals(1, game.howManyPlayers());
+		
+		String output = getOutput(byteOutputStream);
+		Approvals.verify(output);
 	}
 	
 	@Test
-	@UseReporter(DiffReporter.class)
-	public void testGame() {
-		ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
-		PrintStream printStream = new PrintStream(byteOutputStream);
-		System.setOut(printStream);
+	public void testTwoTurnsWithOneWrongAnswer() {
+		ByteArrayOutputStream byteOutputStream = setCustomOutStream();
 		
 		Game game = new Game();
 		addTwoPlayers(game);
@@ -54,10 +57,127 @@ public class SomeTest {
 		game.roll(1);
 		game.wasCorrectlyAnswered();
 		
-		System.out.flush();
-		String output = byteOutputStream.toString();
+		String output = getOutput(byteOutputStream);
 		
 		Approvals.verify(output);
+	}
+	
+	@Test
+	public void testTwoTurnsWithOneWrongAnswerButRollEvenNumber() {
+		ByteArrayOutputStream byteOutputStream = setCustomOutStream();
+		
+		Game game = new Game();
+		addTwoPlayers(game);
+		// first turn
+		game.roll(1);
+		game.wasCorrectlyAnswered();
+		game.roll(1);
+		game.wrongAnswer();
+		// second turn
+		game.roll(1);
+		game.wasCorrectlyAnswered();
+		game.roll(2);
+		game.wasCorrectlyAnswered();
+		
+		String output = getOutput(byteOutputStream);
+		
+		Approvals.verify(output);
+	}
+	
+	@Test
+	public void testTwoTurnsWithOneWrongAnswerAndRollEqualEleven() {
+		ByteArrayOutputStream byteOutputStream = setCustomOutStream();
+		
+		Game game = new Game();
+		addTwoPlayers(game);
+		// first turn
+		game.roll(11);
+		game.wasCorrectlyAnswered();
+		game.roll(1);
+		game.wrongAnswer();
+		// second turn
+		game.roll(1);
+		game.wasCorrectlyAnswered();
+		game.roll(11);
+		game.wasCorrectlyAnswered();
+		
+		String output = getOutput(byteOutputStream);
+		
+		Approvals.verify(output);
+	}
+	
+	@Test
+	public void testTwoTurnsWithOneWrongAnswerForFirstPlayerAndRollEqualEleven() {
+		ByteArrayOutputStream byteOutputStream = setCustomOutStream();
+		
+		Game game = new Game();
+		addTwoPlayers(game);
+		// first turn
+		game.roll(11);
+		game.wrongAnswer();
+		game.roll(1);
+		game.wrongAnswer();
+		// second turn
+		game.roll(1);
+		game.wasCorrectlyAnswered();
+		game.roll(2);
+		game.wasCorrectlyAnswered();
+		
+		String output = getOutput(byteOutputStream);
+		
+		Approvals.verify(output);
+	}
+	
+	@Test
+	public void testTwoTurnsWithOneWrongAnswerForFirstPlayerAndNotLeavingPenaltyBoxAndRollEqualEleven() {
+		ByteArrayOutputStream byteOutputStream = setCustomOutStream();
+		
+		Game game = new Game();
+		addTwoPlayers(game);
+		// first turn
+		game.roll(11);
+		game.wrongAnswer();
+		game.roll(1);
+		game.wasCorrectlyAnswered();
+		// second turn
+		game.roll(2);
+		game.wasCorrectlyAnswered();
+		game.roll(2);
+		game.wasCorrectlyAnswered();
+		
+		String output = getOutput(byteOutputStream);
+		
+		Approvals.verify(output);
+	}
+	
+	@Test
+	public void shouldPlayerLoseWhenPurseEqualsSix() {
+		ByteArrayOutputStream byteOutputStream = setCustomOutStream();
+		
+		Game game = new Game();
+		addTwoPlayers(game);
+
+		for(int i = 0; i < 20; i++) { // roll 12 times
+			game.roll(1);
+			game.wasCorrectlyAnswered();
+		}
+		
+		String output = getOutput(byteOutputStream);
+		
+		Approvals.verify(output);
+	}
+
+	private String getOutput(ByteArrayOutputStream byteOutputStream) {
+		System.out.flush();
+		String output = byteOutputStream.toString();
+		return output;
+	}
+
+	private ByteArrayOutputStream setCustomOutStream() {
+		ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
+		PrintStream printStream = new PrintStream(byteOutputStream);
+		System.setOut(printStream);
+		return byteOutputStream;
 	}
 	
 	private void addTwoPlayers(Game game) {
